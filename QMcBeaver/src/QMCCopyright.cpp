d5ca1c58f784fb0a5ece92c255d20593b811b3d5
drkent <>
2003-02-18 18:39:17 +0000

    Added the caltech copyright info to the program's output.

--- QMcBeaver/src/QMCCopyright.cpp
+++ QMcBeaver/src/QMCCopyright.cpp
@@ -48,6 +48,44 @@ ostream & operator<<(ostream & strm, QMCCopyright & rhs)
   text += "--------------------------------------------\n";
   text += "\n";
 
+  // Caltech copyright info
+  text += "Copyright 2003 California Institute of Technology.\n"; 
+  text += "\n";
+  text += "To contact the original authors, write to: \n";
+  text += "drkent@users.sourceforge.net\n";
+  text += "or\n";
+  text += "mtfeldmann@users.sourceforge.net   \n";
+  text += "\n";
+  text += "\n";
+  text += "This program is free software; you can redistribute it and/or modify\n";
+  text += "it under the terms of the GNU General Public License as published by\n";
+  text += "the Free Software Foundation and so long as the above copyright\n";
+  text += "notice, this paragraph and the following three paragraphs appear in\n";
+  text += "all copies.  \n";
+  text += "\n";
+  text += "This program is distributed in the hope that it will be useful, but\n";
+  text += "WITHOUT ANY WARRANTY; without even the implied warranty of\n";
+  text += "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. In no event \n";
+  text += "shall California Institute of Technology or the authors be liable \n";
+  text += "to any party for direct, indirect, special, incidental or\n";
+  text += "consequential damages, including lost profits, arising out of the use\n";
+  text += "of this software and its documentation, even if the California\n";
+  text += "Institute of Technology or the authors have been advised of the\n";
+  text += "possibility of such damage. Lastly, the California Institute of\n";
+  text += "Technology and the authors have no obligations to provide maintenance,\n";
+  text += "support, updates, enhancements or modifications.   \n";
+  text += "\n";
+  text += "To receive a copy of the GNU General Public License, go to:\n";
+  text += "\n";
+  text += "http://www.gnu.org/licenses/gpl.txt\n";
+  text += "or write to:\n";
+  text += "The Free Software Foundation, Inc.\n";
+  text += "59 Temple Place, Suite 330\n";
+  text += "Boston, MA 02111--1307 USA\n";
+  text += "\n";
+  text += "--------------------------------------------\n";
+  text += "\n";
+
   strm << text;
   return strm;
 }
