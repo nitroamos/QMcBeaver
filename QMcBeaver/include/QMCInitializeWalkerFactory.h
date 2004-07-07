f6cebb2cc701659b484870d792901568b2a8b0cd
madmartigan81 <>
2004-07-07 17:46:22 +0000

    Added dans_walker_initialization as a possible method

--- QMcBeaver/include/QMCInitializeWalkerFactory.h
+++ QMcBeaver/include/QMCInitializeWalkerFactory.h
@@ -17,9 +17,9 @@
 
 #include "QMCMikesJackedWalkerInitialization.h"
 #include "QMCMikesBetterWalkerInitialization.h"
+#include "QMCDansWalkerInitialization.h"
 
 using namespace std;
-
 
 /** 
   Object factory which returns the correct QMCInitialize walker when a 
@@ -29,10 +29,10 @@ using namespace std;
 class QMCInitializeWalkerFactory
 {
 public:
+
   /** 
     Returns the correct QMCInitializeWalker when a string keyword 
     describing the initialization method is provided.
-
     @param input input input data for the calculation
     @param type string describing which initialization algorithm to choose
     @return the selected QMCInitializeWalker method.
