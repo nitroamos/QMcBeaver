59b9b75e2361cfcac7a952a0477c34e1fe92258f
madmartigan81 <>
2004-07-07 17:53:34 +0000

    The initialization stopwatch now times initialization, added an equilibration stopwatch to time the equilibration phase of the calculation.

--- QMcBeaver/include/QMCStopwatches.h
+++ QMcBeaver/include/QMCStopwatches.h
@@ -24,12 +24,13 @@ using namespace std;
 /**
   A collection of Stopwatch objects used to record information
   relevant to the timing of a QMC calculation.
-  */
+*/
 
 class QMCStopwatches
 {
  private:
   Stopwatch Initialization;
+  Stopwatch Equilibration;
   Stopwatch Propagation;
   Stopwatch Communication_send;
   Stopwatch Communication_reduce;
@@ -42,30 +43,41 @@ class QMCStopwatches
 
   /**
     Creates a new instance of this class with all timers stopped.
-    */
+  */
+
   QMCStopwatches();
 
   /**
     Stops all stopwatches in this object which are running.
-    */
+  */
+
   void stop();
 
   /**
     Resets all stopwatches in this object and leaves the stopwatches stopped.
-    */
+  */
+
   void reset();
 
   /**
     Gets the stopwatch which times the initialization of the calculation.
-    */
+  */
+
   Stopwatch * getInitializationStopwatch();
+
+  /**
+    Gets the stopwatch that times the equilibration phase of the calc.
+  */
+
+  Stopwatch * getEquilibrationStopwatch();
 
   /**
     Gets the stopwatch which times the useful propagation of walkers.
     The time required to initialize the walkers is not included.
 
     @return the stopwatch.
-    */
+  */
+
   Stopwatch * getPropagationStopwatch();
 
   /**
@@ -73,7 +85,8 @@ class QMCStopwatches
     processors.
 
     @return the stopwatch.
-    */
+  */
+
   Stopwatch * getSendCommandStopwatch();
 
   /**
@@ -81,14 +94,16 @@ class QMCStopwatches
     all processors.
 
     @return the stopwatch.
-    */
+  */
+
   Stopwatch * getGatherPropertiesStopwatch();
 
   /**
     Gets the stopwatch which times the synchronization of all the processors.
 
     @return the stopwatch.
-    */
+  */
+
   Stopwatch * getCommunicationSynchronizationStopwatch();
 
   /**
@@ -96,68 +111,70 @@ class QMCStopwatches
     processor has a command waiting for it.
 
     @return the stopwatch.
-    */
+  */
+
   Stopwatch * getCommandPollingStopwatch();
 
   /**
     Gets the stopwatch which times the VMC optimization.
 
     @return the stopwatch.
-    */
+  */
+
   Stopwatch * getOptimizationStopwatch();
 
   /**
     Gets the stopwatch which records the total time of the calculation.
 
     @return the stopwatch.
-    */
+  */
+
   Stopwatch * getTotalTimeStopwatch();
 
   /**
      Sets two object equal.
-    */
+  */
 
   void operator = ( const QMCStopwatches &rhs);
 
   /**
     Returns a QMCStopwatches which is the sum of two QMCStopwatches objects.
-    */
+  */
+
   QMCStopwatches operator+(QMCStopwatches & rhs);
 
   /**
     Writes the timing results of this class to a human readable
     stream.
-    */ 
+  */ 
+
   friend ostream& operator <<(ostream& strm, QMCStopwatches & rhs);
-
-
 
 #ifdef PARALLEL
 
 private:
+
   /**
     A flag which tells if MPI_TYPE has been generated.
-    */
+  */
 
   static bool mpiTypeCreated;
-
   
   /**
     Build MPI_TYPE.
-    */
+  */
 
   static void buildMpiType();
 
   /**
     Build MPI_REDUCE.
-    */
+  */
 
   static void buildMpiReduce();
 
-
   /**
     An MPI function which allows MPI_Reduce to be used in adding Stopwatches.
-    */
+  */
 
   static void Reduce_Function(QMCStopwatches *in, QMCStopwatches *inout, 
                               int *len, MPI_Datatype *dptr);
@@ -166,13 +183,13 @@ public:
 
   /**
     The MPI data type for a QMCStopwatches.
-    */
-  static MPI_Datatype MPI_TYPE;
+  */
 
+  static MPI_Datatype MPI_TYPE;
 
   /** 
     The MPI operation for performing MPI_Reduce on QMCStopwatches objects.
-    */
+  */
   
   static MPI_Op MPI_REDUCE;
 
