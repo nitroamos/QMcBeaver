850518125e652078b413d89c7041c85ceb6e8745
frozenflip <>
2005-09-06 23:13:26 +0000

    small perf improvements

--- QMcBeaver/include/Stopwatch.h
+++ QMcBeaver/include/Stopwatch.h
@@ -17,29 +17,67 @@
 
 #include <sys/timeb.h>
 #include <sys/types.h>
+//#include <winsock.h>
+#include <windows.h>
 #define USE_HIGH_PRECISION
 
 struct timezone {
     int tz_minuteswest;
     int tz_dsttime;
 };
-/*
+
+//timeval is defined in winsock, but cygwin's version seems to have a problem...
+//included here just in case winsock doesn't play nice.
+#if defined(__CYGWIN__)
 struct timeval {
         long    tv_sec;
         long    tv_usec;
 };
+#endif
+
+/**
+  High precision timing is not available to Windows through time.h, and I don't
+  know how precise it is on Linux, Unix, etc. This function has been "overloaded"
+  to use special Windows API when it is running on Windows/cygwin. This precision
+  is probably only important for GPU related timings. When I get to Linux,
+  I'll figure out how to make a function that will work there as well.
+  
+  For Intel Pentiums, there is a command called rdtsc. It's possible this works on
+  AMD cpus also. From the Intel software forums, this was recommended as sample code:
+  
+  unsigned __int64 tick = 0;
+  void getTick(void)
+  { 
+     __asm{
+        _emit 0x0f
+        _emit 0x31
+        lea ecx,tick
+        mov dword ptr [ecx],  eax
+        mov dword ptr [ecx+4],edx
+      }
+  }
+
+  main() {
+    while (1) {
+      getTick();
+      printf("%I64u\n", tick);
+    } 
+  }
+
+  I believe this code would have to be matched with a frequency number to turn the number
+  of ticks into an actual time.
 */
 static void gettimeofday(struct timeval* t,void* timezone){
 #ifdef USE_HIGH_PRECISION
-	LARGE_INTEGER timeLI, freqLI;
-	QueryPerformanceCounter(&timeLI);
-	QueryPerformanceFrequency(&freqLI);
-	t->tv_sec=0;
-	double temp = 1.0e6*(double)(timeLI.QuadPart)/freqLI.QuadPart;
+  LARGE_INTEGER timeLI, freqLI;
+  QueryPerformanceCounter(&timeLI);
+  QueryPerformanceFrequency(&freqLI);
+  t->tv_sec=0;
+  double temp = 1.0e6*(double)(timeLI.QuadPart)/freqLI.QuadPart;
   t->tv_usec=(long)(temp + 0.5);
 #else
   struct _timeb timebuffer;
-	_ftime( &timebuffer );
+  _ftime( &timebuffer );
   t->tv_sec=(long)timebuffer.time;
   t->tv_usec=1000*timebuffer.millitm;
 #endif
