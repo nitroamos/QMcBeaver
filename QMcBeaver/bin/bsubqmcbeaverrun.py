0c4f30a205dfffc871ec9f44426508b9d9adce63
frozenflip <>
2006-05-16 01:57:30 +0000

    adding a script useful for LANL QSC

---
+++ QMcBeaver/bin/bsubqmcbeaverrun.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+
+#            QMcBeaver
+#
+#         Constructed by 
+#
+#     Michael Todd Feldmann 
+#              and 
+#   David Randall "Chip" Kent IV
+#
+# Copyright 2000.  All rights reserved.
+#
+# drkent@users.sourceforge.net mtfeldmann@users.sourceforge.net
+
+import sys
+import string
+import os
+
+# FLAGS
+# r - run
+
+if len(sys.argv) < 2: 
+	print "pbsqmcbeaverrun.py <exe> <filename> <number of nodes> <processors per node> <queue name>"
+	sys.exit(0)
+
+exe        = sys.argv[1]
+filename   = sys.argv[2]   
+
+if len(sys.argv) > 3:
+	nodes      = sys.argv[3]
+else:
+	nodes      = "1"
+
+if len(sys.argv) > 4:
+	ppn        = sys.argv[4]
+else:
+	ppn        = "4"
+
+if len(sys.argv) > 5:
+	queue      = sys.argv[5]
+else:
+#	queue      = "largeq"
+	queue      = "smallq"
+
+processors = string.atoi(nodes)*string.atoi(ppn)
+
+base = filename[:-4]
+file = open(base+'run','w')
+ckmf_file = open(filename,'r')
+ckmf_data = ckmf_file.readlines()
+ckmf_file.close()
+
+for i in range(len(ckmf_data)):
+	if string.find(ckmf_data[i],"temp_dir") == 0:
+		temp_dir = ckmf_data[i+1][:-1]
+		break
+
+print "Using temp space: " + temp_dir
+
+debug = 0
+#debug = 1
+
+file.write("#!/bin/csh    \n")
+file.write("#BSUB -J " + filename +        " \n")
+file.write("#BSUB -n " + str(processors) + " \n")
+if debug == 1:
+	file.write("#BSUB -I                         \n")
+	file.write("#BSUB -W 0:30                    \n")
+else:
+	file.write("#BSUB -B                         \n")
+	file.write("#BSUB -W 8:00                    \n")
+file.write("#BSUB -q " + queue + "           \n")
+file.write("#BSUB -wa 'USR1' -wt '10'        \n")
+file.write("\n")
+file.write("cd " + os.getcwd() + "\n")
+if debug == 1:
+	file.write("module load totalview_default\n")
+
+if processors == 1:
+	file.write(exe + " " + filename + " >& " + base + "out \n")
+else:
+	if debug == 1:
+		file.write("totalview prun -a -t " + " " + exe + " " + filename +  " >& " + base + "out\n")
+	else:
+		file.write("prun " + " " + exe + " " + filename +  " >& " + base + "out\n")
+
+file.close()
+
+print "Submitting " + filename + " to " + str(processors) + " processors on " + queue
+command = "bsub < " + base +"run"
+print command
+os.system(command)
+
