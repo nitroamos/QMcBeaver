3892f2dab17386b8456187d5d56d2db74837c819
frozenflip <>
2007-10-11 23:34:25 +0000

    this works on coyote, and not on qsc

--- QMcBeaver/bin/bsubqmcbeaverrun.py
+++ QMcBeaver/bin/bsubqmcbeaverrun.py
@@ -18,7 +18,8 @@ import os
 
 default_nodes = 1
 default_ppn   = 4
-default_queue = "smallq"
+default_hours = 12
+default_queue = "largeq"
 
 emailaddress = "your@address.edu"
 
@@ -32,6 +33,13 @@ if len(sys.argv) < 2:
 exe        = sys.argv[1]
 filename   = sys.argv[2]   
 
+if not os.path.isfile(filename):
+    print "This is not a valid input file: " + filename
+    sys.exit(0)
+if not os.path.isfile(exe):
+    print "This is not a valid executable: " + exe
+    sys.exit(0)
+    
 if len(sys.argv) > 3:
 	nodes      = string.atoi(sys.argv[3])
 else:
@@ -43,6 +51,11 @@ else:
 	ppn        = default_ppn
 
 if len(sys.argv) > 5:
+      hours        = string.atoi(sys.argv[5])
+else:
+      hours        = default_hours
+
+if len(sys.argv) > 6:
 	queue      = sys.argv[5]
 else:
 	queue      = default_queue
@@ -73,7 +86,8 @@ if debug == 1:
 	file.write("#BSUB -W 0:30                    \n")
 else:
 	file.write("#BSUB -B                         \n")
-	file.write("#BSUB -W 8:00                    \n")
+	#file.write("#BSUB -W 8:00                    \n")
+	file.write("#BSUB -W "+ str(hours) + ":00\n")
 file.write("#BSUB -q " + queue + "           \n")
 file.write("#BSUB -wa 'URG' -wt '10'        \n")
 file.write("\n")
@@ -84,15 +98,15 @@ if debug == 1:
 if processors == 1:
 	file.write(exe + " " + filename + " >& " + base + "out \n")
 else:
-	if debug == 1:
-		file.write("totalview prun -a -t " + " " + exe + " " + filename +  " >& " + base + "out\n")
-	else:
-		file.write("prun " + " " + exe + " " + filename +  " >& " + base + "out\n")
+	file.write("mpirun  -np " + str(processors) + \
+		   " " + exe + " " + filename + \
+		   " >& " + base + "out\n")
+
 if emailaddress != "your@address.edu":
 	file.write("csh -c \"cat " + base + "run; cat " + base + "out;\" | /bin/mailx -s \"[qsc] " + base + "out\" " + emailaddress + "\n\n\n")
 file.close()
 
-print "Submitting " + filename + " to " + str(processors) + " processors on " + queue
+print "Submitting " + filename + " to " + str(processors) + " processors on " + queue + " for " + str(hours) + " hours"
 command = "bsub < " + base +"run"
 print command
 os.system(command)
