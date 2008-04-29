cde8422c8e6ccf0a27f4dc7e723ac2bf94a05c4d
drkent <>
2008-04-29 01:37:01 +0000

    Fixed some problems with generating the documentation during release.

--- QMcBeaver/bin/createrelease.py
+++ QMcBeaver/bin/createrelease.py
@@ -4,9 +4,13 @@ import os
 import os.path
 import string
 
+def execute(command):
+    print command
+    os.system(command)
+
 def copy_qmcbeaver(version):
     command = "cp -r QMcBeaver QMcBeaver-%d"%version
-    os.system(command)
+    execute(command)
 
 ''' 
 Delete the CVS directories.
@@ -14,8 +18,7 @@ Delete the CVS directories.
 def delete_cvs_directories(directory):
 
     if os.path.isdir(directory+"CVS"):
-        print "delete "+directory+"CVS"
-        os.system("rm -rf " + directory+"CVS")
+        execute("rm -rf " + directory+"CVS")
 
     everything = os.listdir(directory)
 
@@ -43,22 +46,26 @@ def set_version_number(version):
 
 def generate_zip_file(version):
     command = "tar -zcvf QMcBeaver-%d.tar.gz QMcBeaver-%d"%(version,version)
-    os.system(command)
+    execute(command)
 
 def generate_docs(version):
     # make doxygen documents
     command = "cd QMcBeaver-%d/docs && doxygen | tee doxy.log"%version
-    os.system(command)
+    execute(command)
     # build doxygen API manual
-    command = "cd QMcBeaver-%d/docs/latex && make"%version
-    os.system(command)
+    command = "cd QMcBeaver-%d/docs/latex && latex refman.tex"%version
+    execute(command)
+    command = "cd QMcBeaver-%d/docs/latex && latex refman.tex"%version
+    execute(command)
+    command = "cd QMcBeaver-%d/docs/latex && dvipdf refman.dvi"%version
+    execute(command)
     command = "mv QMcBeaver-%d/docs/latex/refman.pdf QMcBeaver-API_manual-%d.pdf"%(version,version)
-    os.system(command)
+    execute(command)
     # build user guide
     command = "cd QMcBeaver-%d/docs && pdflatex beaver"%version
-    os.system(command)
+    execute(command)
     command = "mv QMcBeaver-%d/docs/beaver.pdf QMcBeaver-user_guide-%d.pdf"%(version,version)
-    os.system(command)
+    execute(command)
     
 
 if __name__ == '__main__':
